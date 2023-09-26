93ee25acf48aab86a1769cd45411cfac4de3fd51
GloamingCN <729007894@qq.com>
2023-09-26 16:08:22 +0800

    初始化项目。

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import '@arco-design/web-vue/dist/arco.css';
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import ArcoVue from '@arco-design/web-vue';
+
+const app = createApp(App)
+
+app.use(router)
+app.use(ArcoVue);
+
+app.mount('#app')
